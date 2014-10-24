c7fe7d839d7bfdebc8fbb198ec54aed23889cdbc
calvinchu8172 <calvinchu8172@gmail.com>
2014-10-24 10:31:05 +0800

    Deploy to Heroku

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,15 @@ before_action :set_event, :only => [ :show, :edit, :update, :destroy]
 # end
 def index
   @events = Event.page(params[:page]).per(5)
+
+  Rails.logger.debug("event: #{@event.inspect}") #check events
+
+  respond_to do |format|
+    format.html # index.html.erb
+    format.xml { render :xml => @events.to_xml }
+    format.json { render :json => @events.to_json }
+    format.atom { @feed_title = "My event list" } # index.atom.builder
+  end
 end
 
 def new
@@ -23,7 +32,14 @@ end
 def show
   # @event = Event.find(params[:id])
   # @event = Event.find(params[:id])
-  @page_title = @event.name
+  # @page_title = @event.name
+@event = Event.find(params[:id])
+  respond_to do |format|
+    format.html { @page_title = @event.name } # show.html.erb
+    format.xml # show.xml.builder
+    format.json { render :json => { id: @event.id, name: @event.name }.to_json }
+  end
+
 end
 
 def edit
